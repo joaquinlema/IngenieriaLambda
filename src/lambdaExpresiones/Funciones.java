e54110b654f72de133225146514dc636eed3cfde
joaquin lema <joaco.lema@hotmail.com>
2017-09-30 17:33:11 -0300

    primer funcion lambda tipo filter

--- src/lambdaExpresiones/Funciones.java
+++ src/lambdaExpresiones/Funciones.java
@@ -1,5 +1,39 @@
 package lambdaExpresiones;
 
-public class Funciones {
+import java.util.List;
+import java.util.function.BiFunction;
 
+public class Funciones {
+	
+	public static int maximo(List<Integer> lista){
+		int max = lista.get(0);
+		for(int elem :  lista){
+			if(elem > max) max = elem;
+		}
+		return max;
+	}
+	
+	public static int minimo(List<Integer> lista){
+		int min = lista.get(0);
+		for(int elem :  lista){
+			if(elem < min) min = elem;
+		}
+		return min;
+	}
+	
+	public static int operar(BiFunction<Integer,Integer, Boolean> operacion,List<Integer> lista){
+		int max = lista.get(0);
+		for(int elem :  lista){
+			if(operacion.apply(elem,max)) max = elem;
+		}
+		return max;
+	}
+	
+	public static int max(List<Integer> lista){
+		return operar((elem,max)-> elem > max , lista);
+	}
+	
+	public static int min(List<Integer> lista){
+		return operar((elem,elem2)-> elem < elem2 , lista);
+	}
 }
--- src/lambdaExpresiones/Test/TestLambda.java
+++ src/lambdaExpresiones/Test/TestLambda.java
@@ -2,13 +2,43 @@ package lambdaExpresiones.Test;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
 import org.junit.Test;
 
-public class TestLambda {
+import lambdaExpresiones.Funciones;
 
+public class TestLambda {
+	List<Integer> miLista = new ArrayList<>();
+	
+	@Before
+	public void iniciar(){
+		miLista.add(1);
+		miLista.add(3);
+		miLista.add(9);
+		miLista.add(5);
+	}
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testMaximo() {
+		assertEquals(Funciones.maximo(miLista),9 );
+	}
+	
+	@Test
+	public void testMax() {
+		assertEquals(Funciones.max(miLista),9 );
+	}
+	
+	@Test
+	public void testMinimo() {
+		assertEquals(Funciones.minimo(miLista),1 );
+	}
+	
+	@Test
+	public void testMin() {
+		assertEquals(Funciones.min(miLista),1 );
 	}
 
 }
