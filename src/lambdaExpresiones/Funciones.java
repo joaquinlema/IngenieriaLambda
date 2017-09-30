3511f691ba223e3fea49bbea24f444db5c56d7ec
joaquin lema <joaco.lema@hotmail.com>
2017-09-30 16:44:57 -0300

    ccommit 2

---
+++ src/lambdaExpresiones/Funciones.java
@@ -0,0 +1,5 @@
+package lambdaExpresiones;
+
+public class Funciones {
+
+}
---
+++ src/lambdaExpresiones/Test/TestLambda.java
@@ -0,0 +1,14 @@
+package lambdaExpresiones.Test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestLambda {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
